c8098f2ec1ca44e778b2760c36bc700a0a5ba6e8
ZefanHu <1371738978.com>
2024-09-30 00:23:30 +0800

    修复数据库bug,优化提示及查询语句

--- qa_server.py
+++ qa_server.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 from flask import request
 from service.question_service import question_service_instant
-
+from common import log_util
 
 app = Flask("KGQASE")
+log = log_util.get_main_logger()
 
 def start_server():
     app.run(host='0.0.0.0', port=5000)
 
-
-@app.route("/web")
+@app.route("/")
 def web():
+    log.warning("====== user ip: {} index======".format(request.remote_addr))
     answer = {
         "content" : "",
         "question" : ""
@@ -19,6 +20,7 @@ def web():
 
 @app.route("/web_answer", methods=["GET","POST"])
 def web_answer():
+    log.warning("====== user ip: {} get_answer======".format(request.remote_addr))
     question = request.form["question"]
     answer_str = question_service_instant.get_answer(question)
     answer = {
